167ae791df26c0972ba8418aefca180ee3300517
KevDoesntDev <keviscool@zohomail.com>
2023-02-04 09:55:29 -0800

    yw anthony

--- include/devices.hpp
+++ include/devices.hpp
@@ -1,11 +1,4 @@
-#pragma once
 #include "main.h"
-#include "okapi/impl/device/button/adiButton.hpp"
-#include "pros/adi.hpp"
-#include "pros/motors.hpp"
+#pragma once
+
 extern Controller controller;
-extern ADIButton limitSwitch;
-extern Motor catapult;
-extern Motor intake;
-extern IMU inertial;
-extern Motor expansion;
